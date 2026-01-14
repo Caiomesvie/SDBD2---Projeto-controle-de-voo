0c293f29b3e09c3f66f0ebfd789e38a33d087190
ciro-c <ciro.fga@gmail.com>
2026-01-14 10:01:09 -0300

    Fix

--- raw/data_raw/join.bat
+++ raw/data_raw/join.bat
@@ -2,7 +2,7 @@
 echo Juntando Microdados partes no MICRODADOS_ENEM_2021.csv...
 echo.
 
-copy /b "MicrodadosEnem-spli-*" "MICRODADOS_ENEM_2021.csv"
+copy /b "MicrodadosEnem-split-*" "MICRODADOS_ENEM_2021.csv"
 
 echo.
 echo concluido!
